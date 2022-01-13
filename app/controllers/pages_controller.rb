df655f3cccbf82bb8df6436e1502d777cbee400a
Talha Qureshi <talha_qureshi@hotmail.com>
2022-01-13 17:53:04 +0500

    cosmetic changes, navbar added

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,14 +1,17 @@
 class PagesController < ApplicationController
-    def fall_shows
+    def home
         @anime_titles = Show.all
+    end
+    def fall_shows
+        @anime_titles = Show.where(season: "fall-22")
     end
     def spring_shows
-        @anime_titles = Show.all
+        @anime_titles = Show.where(season: "spring-22")
     end
     def summer_shows
-        @anime_titles = Show.all
+        @anime_titles = Show.where(season: "summer-22")
     end
     def winter_shows
-        @anime_titles = Show.all
+        @anime_titles = Show.where(season: "winter-22")
     end 
 end
